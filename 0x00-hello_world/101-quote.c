4f8b7d53dffb8e22334f9df42ae989a8870e056f
SARK <kosinick01@gmail.com>
2023-03-16 20:44:18 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -print a string without the functions printf or puts
+ *
+ * Return: Always 1
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
