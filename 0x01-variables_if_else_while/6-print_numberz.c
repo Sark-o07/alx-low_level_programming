9f7936dc69c5ff1b6f8648ae9a8c4ba589bc1544
SARK <kosinick01@gmail.com>
2023-03-17 21:36:47 -0700

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,11 +3,12 @@
  * main -prints all single digit numbers of base 10 starting from 0
  * Return: 0;
 */
+
 int main(void)
 {
-	char i;
+	int i;
 
-	for (i = "0"; i <= "10"; i++)
+	for (i = 0; i <= 10; i++)
 	{
 		putchar(i);
 	}
