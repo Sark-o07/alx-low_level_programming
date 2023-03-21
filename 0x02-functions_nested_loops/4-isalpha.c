97b64248731be2654c8f15ea2c224c185ed2e19c
SARK <kosinick01@gmail.com>
2023-03-21 16:56:05 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_isalpha -checks for alphabetic character.
+ *@c: is the car to be checked.
+ * Return: 1 if char is alphabet, else 0.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
