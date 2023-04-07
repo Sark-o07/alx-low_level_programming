ca1fdf513a8124641d486ecbdae62e78aa23c482
SARK <kosinick01@gmail.com>
2023-04-06 20:35:38 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,3 +1,17 @@
-#include "maian.h"
+#include "main.h"
 /**
- * 
+ *_puts_recursion - prints a string, followed by a new line.
+ *@s: the string to be printed
+ *Return: void
+ **/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(10);
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
