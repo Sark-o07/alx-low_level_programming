674c584034e1de4b5fc8da1a6686bdc62fa45de2
SARK <kosinick01@gmail.com>
2023-04-06 18:26:21 -0700

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,33 @@
 #include "main.h"
 /**
- **/
+*sqrt_checker - checks if a number is a perfect square
+*@N: integer to be checked
+*@count: the variable that holds the root
+*Return: the root or -1
+**/
+int sqrt_checker(int N, int count)
+{
+	int diff = N - count * count;
+
+	if (diff < 0)
+		return (-1);
+	else if (diff == 0)
+		return (count);
+	else
+		return (sqrt_checker(N, count + 1));
+}
+int sqrt_checker(int N, int count);
+/**
+* _sqrt_recursion - a function that returns the square root
+*@n: integer to be checked
+*Return: the square root of the parameter or -1
+**/
 int _sqrt_recursion(int n)
 {
-	int count = 1, diff = 0;
-
-	if (n == 1)
-		return (1);
-	else if (n == 0)
-		return (0);
-	else if (diff < 0)
+	if (n < 0)
 		return (-1);
-	diff = n - count;
-	return(1 + _sqrt_recursion(diff - (count + 2)));
+	if (n == 0 || n == 1)
+		return (n);
+	else
+		return (sqrt_checker(n, 1));
 }
-			
-			
-	
