34986521df4901657a209b8a5c10ece4011507c2
SARK <kosinick01@gmail.com>
2023-04-10 05:10:10 -0700

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 /**
  * main - prints the command name
+ *@argc: the size of an array argsv
+ *@argsv: an arry containing the program
+ *command line argument
  * Return: 0
  **/
 int main(int argc, char *argsv[])
