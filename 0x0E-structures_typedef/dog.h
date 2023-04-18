b76c3fecf52b763291c7862ccaa5082b1c05a548
SARK <kosinick01@gmail.com>
2023-04-18 13:50:19 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a new type struct dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: a self-defined data type for dog info
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
