f49616c963ca331ed5cf87a145acded5eab4eecf
SARK <kosinick01@gmail.com>
2023-04-27 16:23:11 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+* print_list- prints all the elements of a list_t list
+* @h: a struct pointer
+* Return: then number of nodes
+**/
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	for(; h != NULL;)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		s++;
+	}
+	return (s);
+}
