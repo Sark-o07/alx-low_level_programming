4bb19f3ade6ce135dd88007292341670ef8a1b42
SARK <kosinick01@gmail.com>
2023-05-05 16:50:47 -0700

    6. Endianness

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,43 +1,23 @@
 #include "main.h"
 /**
- * power - self-defined power function
- * @base: the base
- * @exponent: the exponent
- * Return: the result of the function
- **/
-double power(double base, int exponent)
-{
-	double result = 1.0;
-	int i;
-
-	for (i = 0; i < exponent; i++)
-		result *= base;
-
-	return (result);
-}
-/**
- * binary_to_uint - converts a binary number to an unsigned int.
- * @b: the binary to be converted
- * Return:  the converted number, or 0
- * if there is one or more chars in the string b that is not 0 or 1
+ * binary_to_uint - coverts a binary number to an usigned int
+ * @b: is pointing to a string of 0 and 1 chars
+ * Return: the converted number, or 0 if t's any char that is not 0 or 1 |
+ * b is NULL
  **/
 unsigned int binary_to_uint(const char *b)
 {
-	int i, len = 0, expo = 0;
 	unsigned int decimal = 0;
+	int i;
 
 	if (b == NULL)
 		return (0);
-	while (b[len])
-	{
-		len++;
-	}
-	for (i = len - 1; i >= 0; i--)
+
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		decimal += (b[i] - '0') * power(2, expo);
-		expo++;
+		decimal = 2 * decimal + (b[i] - '0');
 	}
 	return (decimal);
 }
