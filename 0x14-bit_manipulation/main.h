2f7417134cb6a999484cdc871860f8ca6d9cbdce
SARK <kosinick01@gmail.com>
2023-05-05 16:51:03 -0700

    6. Endianness

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,10 +3,9 @@
 #include <stddef.h>
 #include <stdio.h>
 
-double power(double base, int exponent);
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-
+int get_endianness(void);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
