de69c4cac9c9b590c349c52fb4475cc687f7705d
SARK <kosinick01@gmail.com>
2023-05-04 20:31:58 -0700

    fix

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,7 +5,7 @@
  * @n: the decimal to be operated
  * Return: 1 if it worked, or -1 if an error ocurred.
  **/
-set_bit(unsigned long int *n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int)* 8)
 		return (-1);
