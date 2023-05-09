9847d456224f6b23e556d810d5796d5ccd179d7e
SARK <kosinick01@gmail.com>
2023-05-08 21:00:37 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(file, text_content, size);
 
-	if(file == -1 || wr == -1)
+	if (file == -1 || wr == -1)
 		return (-1);
 	close(file);
 
