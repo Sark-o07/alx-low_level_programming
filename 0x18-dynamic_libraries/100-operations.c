c4cc6fe69ad7f6aad4fe96d1ecee87573f85fa5e
SARK <kosinick01@gmail.com>
2023-08-22 14:23:51 +0100

    function files

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - addition
+ * @a: digit_1
+ * @b: digit_2
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+	return a + b;
+}
+/**
+ * sub - subtraction
+ * @a: digit_1
+ * @b: digit_2
+ * Return: the difference of a and b
+ */
+int sub(int a, int b)
+{
+	return a - b;
+}
+/**
+ * mul - multiplication
+ * @a: digit_1
+ * @b: digit_2
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return a * b;
+}
+/**
+ * div - division
+ * @a: digit_1
+ * @b: digit_2
+ * Return: a divided by b
+ */
+int div(int a , int b)
+{
+	return a / b;
+}
+/**
+ * mod - modulo
+ * @a: digit_1
+ * @b: digit_2
+ * Return: a modulo b
+ */
+int mod(int a, int b)
+{
+	return a % b;
+}
